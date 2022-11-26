c002102df57a5358ac6ba445064f874a62c36e79
Qiers <50917406+Qiers@users.noreply.github.com>
2022-11-26 21:09:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '亲爱的小魏同学',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oqmEU6STTbqZoZJbqLHLZRhabz20',
+      id: 'oqmEU6dhfqiSy3W_jc7-mfZCkp7g',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '6vMxfVIlEvEZ58yQM9ir1fCEWucnrnAT_ACUc2mnQBc',
+      useTemplateId: 'H4VDHrvA8_8zuRyapSmmUMZFGtdQW2ZaTC9vM3guKBs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
