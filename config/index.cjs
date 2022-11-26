226dbc62088bee364c0f8de1f4aaf9330e19a877
Qiers <50917406+Qiers@users.noreply.github.com>
2022-11-26 20:45:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,13 +23,13 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqmEU6STTbqZoZJbqLHLZRhabz20',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	T-nyzzdCXCXRakTBXvXKg4YLopgrBBOfRZuUkLe8Gak',
+      useTemplateId: 'T-nyzzdCXCXRakTBXvXKg4YLopgrBBOfRZuUkLe8Gak',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1999', date: '10-17',
+          type: '*生日', name: '小魏', year: '1999', date: '10-17',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         //{
